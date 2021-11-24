e4d14f4f67b291650cf3ba5141c8bfbfdd22f440
Roque Alejandro Rodríguez <roque.alejandro@hotmail.com>
2021-11-24 18:28:01 -0400

    Updated Certs

--- awsService.js
+++ awsService.js
@@ -1,11 +1,11 @@
 const awsIot = require("aws-iot-device-sdk");
 
 const device  =  awsIot.device({
-  keyPath: './awsCerts/6798dc4986-private.pem.key',
-  certPath: './awsCerts/6798dc4986-certificate.pem.crt',
+  keyPath: './awsCerts/10578-private.pem.key',
+  certPath: './awsCerts/10578-certificate.pem.crt',
   caPath: './awsCerts/AmazonRootCA1.pem',
-  host: 'a2w4letmfk56v9-ats.iot.us-east-1.amazonaws.com',
-  clientId: 'ControladorSemaforo',
+  host: 'a3db0douoabkpk-ats.iot.us-east-1.amazonaws.com',
+  clientId: 'proyectWebIoT',
   region: 'us-east-1',
 });
 
